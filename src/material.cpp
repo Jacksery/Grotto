904aebf3190feed8d368d4a0581230e717446b64
jack <jacksj2020@gmail.com>
2025-09-25 20:08:29 +0100

    feat: configure sampler parameters for texture management

--- src/material.cpp
+++ src/material.cpp
@@ -21,6 +21,13 @@ Material::Material(const char *texturePath) {
     // Upload texture data to GPU
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
                  GL_UNSIGNED_BYTE, data);
+
+    // Configure sampler parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   } else {
     std::cerr << "\033[31m[ERROR::TEXTURE]\033[0m Failed to load texture: "
               << texturePath << std::endl;
